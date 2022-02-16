21b0ef2a1f657c0fc00e3535731dd76ef488c1e5
Joe Hagan <jhaganfilm@gmail.com>
2022-02-17 08:21:55 +0900

    Front end start

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,81 @@
+import { useState } from "react"
+import { FaSignInAlt } from "react-icons/fa"
+import { toast } from "react-toastify"
+import { useSelector, useDispatch } from "react-redux"
+import { login } from "../features/auth/authSlice"
+
+function Login() {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  })
+
+  const { email, password } = formData
+
+  const dispatch = useDispatch()
+
+  const { user, isError, isLoading, isSuccess, message } = useSelector(
+    (state) => state.auth
+  )
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }))
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+
+    const userData = {
+      email,
+      password,
+    }
+
+    dispatch(login(userData))
+  }
+
+  return (
+    <>
+      <section className="heading">
+        <h1>
+          <FaSignInAlt /> Login
+        </h1>
+        <p>Please log in to get support</p>
+      </section>
+      <section className="form">
+        <form onSubmit={onSubmit}>
+          <div className="form-group">
+            <input
+              type="email"
+              name="email"
+              id="email"
+              value={email}
+              onChange={onChange}
+              placeholder="Enter Your Email"
+              required
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              name="password"
+              id="password"
+              value={password}
+              onChange={onChange}
+              placeholder="Enter Your Password"
+              required
+            />
+          </div>
+
+          <div className="form-group">
+            <button className="btn btn-block">Submit</button>
+          </div>
+        </form>
+      </section>
+    </>
+  )
+}
+
+export default Login
---
+++ frontend/src/pages/Register.jsx
@@ -0,0 +1,109 @@
+import { useState } from "react"
+import { FaUser } from "react-icons/fa"
+import { toast } from "react-toastify"
+import { useSelector, useDispatch } from "react-redux"
+import { register } from "../features/auth/authSlice"
+
+function Register() {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    password2: "",
+  })
+
+  const { name, email, password, password2 } = formData
+
+  const dispatch = useDispatch()
+
+  const { user, isError, isLoading, isSuccess, message } = useSelector(
+    (state) => state.auth
+  )
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }))
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+
+    if (password !== password2) {
+      toast.error("Passwords do not match")
+    } else {
+      const userData = {
+        name,
+        email,
+        password,
+      }
+
+      dispatch(register(userData))
+    }
+  }
+
+  return (
+    <>
+      <section className="heading">
+        <h1>
+          <FaUser /> Register
+        </h1>
+        <p>Please Create an Account</p>
+      </section>
+      <section className="form">
+        <form onSubmit={onSubmit}>
+          <div className="form-group">
+            <input
+              type="text"
+              name="name"
+              id="name"
+              value={name}
+              onChange={onChange}
+              placeholder="Enter Your Name"
+              required
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="email"
+              name="email"
+              id="email"
+              value={email}
+              onChange={onChange}
+              placeholder="Enter Your Email"
+              required
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              name="password"
+              id="password"
+              value={password}
+              onChange={onChange}
+              placeholder="Enter Your Password"
+              required
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              name="password2"
+              id="password2"
+              value={password2}
+              onChange={onChange}
+              placeholder="Confirm Password"
+              required
+            />
+          </div>
+          <div className="form-group">
+            <button className="btn btn-block">Submit</button>
+          </div>
+        </form>
+      </section>
+    </>
+  )
+}
+
+export default Register
