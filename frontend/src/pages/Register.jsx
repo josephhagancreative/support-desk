c8a06991c6dc175dd0b3e0e4e082817a4feb23c7
Joe Hagan <jhaganfilm@gmail.com>
2022-02-20 21:54:51 +0900

    Finished Implementation

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,8 +1,10 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
+import { useNavigate } from "react-router-dom"
 import { FaUser } from "react-icons/fa"
 import { toast } from "react-toastify"
 import { useSelector, useDispatch } from "react-redux"
-import { register } from "../features/auth/authSlice"
+import { register, reset } from "../features/auth/authSlice"
+import Spinner from "../components/Spinner"
 
 function Register() {
   const [formData, setFormData] = useState({
@@ -15,10 +17,23 @@ function Register() {
   const { name, email, password, password2 } = formData
 
   const dispatch = useDispatch()
+  const navigate = useNavigate()
 
   const { user, isError, isLoading, isSuccess, message } = useSelector(
     (state) => state.auth
   )
+
+  useEffect(() => {
+    if (isError) {
+      toast.error(message)
+    }
+    // redirect when logged it
+    if (isSuccess || user) {
+      navigate("/")
+    }
+
+    dispatch(reset())
+  }, [isError, isSuccess, user, message, navigate, dispatch])
 
   const onChange = (e) => {
     setFormData((prevState) => ({
@@ -41,6 +56,10 @@ function Register() {
 
       dispatch(register(userData))
     }
+  }
+
+  if (isLoading) {
+    return <Spinner />
   }
 
   return (
